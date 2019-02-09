42f7a78d27a9068811a289ad45a9b4f88a2bcdd5
alessandro <alessandro.benassi7@studio.unibo.it>
2019-02-09 23:56:46 +0100

    list part 1

--- phase1/C/pcb.c
+++ phase1/C/pcb.c
@@ -14,7 +14,7 @@ HIDDEN struct list_head pcbfree_h;
 HIDDEN pcb_t pcb_table[MAX_PROC];
 
 /* Initialize list of free pcbs allocating MAX_PROC of them */
-void initPcbs() {
+extern void initPcbs() {
   uint8_t i;
 
   INIT_LIST_HEAD(&pcbfree_h);   /* Initialize sentinel */
@@ -25,12 +25,12 @@ void initPcbs() {
 }
 
 /* Put p in the list of free pcbs */
-void freePcb(pcb_t *p) {
+extern void freePcb(pcb_t *p) {
   list_add(&(p->p_next), &pcbfree_h);
 }
 
 /* Remove and return one pcb from the free list */
-pcb_t *allocPcb() {
+extern pcb_t *allocPcb() {
   struct list_head *alloc_head;
   pcb_t *alloc;
 
@@ -55,4 +55,54 @@ pcb_t *allocPcb() {
   alloc->p_semKey = NULL;
 
   return alloc;
+}
+
+extern void mkEmptyProcQ(struct list_head *list_head) {
+  INIT_LIST_HEAD(list_head);
+}
+
+extern int emptyProcQ(struct list_head *list_head) {
+  return list_empty(list_head);
+}
+
+extern void insertProcQ(struct list_head *list_head, pcb_t *p) {
+  struct list_head *pos;
+  list_for_each(pos, list_head) {
+    if (list_entry(pos, pcb_t, p_next)->priority < p->priority){
+        list_add_tail(&p->p_next, pos);
+	return;
+    }
+  }
+  /* Priority less than every other: the if is never triggered; add at the end */
+  list_add_tail(&p->p_next, pos);
+}
+
+extern pcb_t *headProcQ(struct list_head *list_head) {
+  if(list_empty(list_head))
+    return NULL;
+
+  pcb_t *p = list_entry(list_head->next, pcb_t, p_next);
+  return p;
+}
+
+extern pcb_t *removeProcQ(struct list_head *list_head) {
+  if(list_empty(list_head))
+    return NULL;
+
+  pcb_t *p = list_entry(list_head->next, pcb_t, p_next);
+  list_del(&(p->p_next));
+  return p;
+}
+
+extern pcb_t *outProcQ(struct list_head *list_head, pcb_t *p) {
+  struct list_head *pos;
+
+  list_for_each(pos, list_head) {
+    if (list_entry(pos, pcb_t, p_next) == p){
+         list_del(pos);
+	 return list_entry(pos, pcb_t, p_next);
+    }
+  }
+
+  return NULL;
 }
