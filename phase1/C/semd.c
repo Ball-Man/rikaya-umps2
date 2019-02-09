a550f162c612fba95b0235f15f92f8bdfec63b80
BallMan <francesco.mistri98@gmail.com>
2019-02-10 00:26:04 +0100

    Semd insertBlocked
    Implemented insertBlocked(not tested yet). Also added two hidden functions to help me with a few implementations.

--- phase1/C/semd.c
+++ phase1/C/semd.c
@@ -26,17 +26,74 @@ extern void initASL() {
     list_add(&((semd_table + i)->s_next), &semdfree_h);
 }
 
+/* Internal utility;
+ * Remove the semd corresponding to key from the ASL and put into the free list
+ * If key isn't found return NULL.
+ */
+HIDDEN semd_t *_freeSemdKey(int *key) {
+  struct semd_t *sem;
+
+  sem = getSemd(key);
+  if (!sem)
+    return NULL;
+
+  /* Remove from ASL*/
+  list_del(&sem->s_next);
+
+  /* Add to the free list */
+  list_add(&sem->s_next, &semdfree_h);
+
+  return sem;
+}
+
+/* Internal utility;
+ * Allocate in the ASL a semd from the free list and set it up with the
+ * given key.
+ * If no free semd is left, return NULL
+ */
+HIDDEN semd_t *_allocSemdKey(int *key) {
+  struct list_head *alloc_head;
+  semd_t *alloc;
+
+  if (list_empty(&semdfree_h))
+    return NULL;
+
+  /* Remove from list */
+  alloc_head = semdfree_h.next;
+  list_del(alloc_head);
+  alloc = list_entry(alloc_head, semd_t, s_next);
+  
+  /* Initialize semd */
+  alloc->s_key = key;
+  mkEmptyProcQ(&alloc->s_procQ);
+
+  /* Add to ASL */
+  list_add(alloc_head, &semd_h);
+
+  return alloc;
+}
+
 /* Get the semd corresponding to the key(from the ASL) */ 
 extern semd_t *getSemd(int *key) {
   struct list_head *pos;
-  semd_t *p;
+  semd_t *sem;
 
   list_for_each(pos, &semd_h) {    /* Search through the list */
-    p = list_entry(pos, semd_t, s_next);
-    if (p->s_key == key)
-      return p;
+    sem = list_entry(pos, semd_t, s_next);
+    if (sem->s_key == key)
+      return sem;
   }
   return NULL;    /* If key isn't found */
 }
 
+/* Insert p in the queue of semd corresponding to the given key */
+extern bool insertBlocked(int *key, pcb_t *p) {
+  semd_t *sem = getSemd(key);
 
+  if (!sem)
+    if (!(sem = _allocSemdKey(key))) /* If can't allocate a new semd */
+      return true;
+
+  insertProcQ(&sem->s_procQ, p);
+  return false;
+}
