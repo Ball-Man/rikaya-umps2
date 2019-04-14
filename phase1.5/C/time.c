5c4cce0930e0b068253ac02f7f4cf7b00f183cd0
BallMan <francesco.mistri98@gmail.com>
2019-04-14 19:17:31 +0200

    Time module update
    Added get_timer_value(uint) which asks for ms and returns the equivalent in cpu cycles(used to set the local timer at the correct time slice).

--- phase1.5/C/time.c
+++ phase1.5/C/time.c
@@ -9,6 +9,11 @@ extern unsigned int get_microseconds()
   return TOD_LO / TIME_SCALE;
 }
 
+/* Get the number of cpu cycles corresponding to a given amount of ms */
+extern unsigned int get_timer_value(unsigned int ms) {
+  return ms * 1000 * TIME_SCALE;
+}
+
 /* Time delay */
 extern void delay_ms(unsigned int ms)
 {
--- phase1.5/E/time.e
+++ phase1.5/E/time.e
@@ -4,6 +4,9 @@
 /* Get number of microseconds passed */
 extern unsigned int get_microseconds();
 
+/* Get the number of cpu cycles corresponding to a given amount of ms */
+extern unsigned int get_timer_value(unsigned int ms);
+
 /* Time delay */
 extern void delay_ms(unsigned int ms);
 
