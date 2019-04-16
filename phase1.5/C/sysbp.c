ecca6732dc978dc08a4f15bf2a95db26ddc16f73
BallMan <francesco.mistri98@gmail.com>
2019-04-16 15:57:54 +0200

    SYS3
    SYS3 is the only managed system call for this phase. It terminates and removes from the ready queue the current process and all its progeny.

--- phase1.5/C/sysbp.c
+++ phase1.5/C/sysbp.c
@@ -4,13 +4,43 @@
 #include <terminal.e>
 #include <umps/libumps.h>
 #include <umps/arch.h>
+#include <scheduler.e>
+#include <pcb.e>
 
 /* Main handler for system calls and breakpoints */
 extern void sysbp() {
   state_t *old_area = (state_t *)SYSBP_OAREA;
 
-  term_printf("SYSCALL/BP\nCause: %d\nNumber: %d\n", old_area->cause, old_area->reg_a0);
+  sys3();
 
   old_area->pc_epc += WORD_SIZE;
   LDST(old_area);
+}
+
+/* Handler for sys3: terminate current process and its tree */
+extern void sys3() {
+  struct list_head q;
+  struct list_head *pos;
+  pcb_t *parent;
+
+  mkEmptyProcQ(&q);
+
+  list_add_tail(&outProcQ(&ready_queue, cur_proc)->p_next, &q);
+
+  while ((parent = removeProcQ(&q))) {
+    /* Check if the children queue is empty or not defined */
+    if (!emptyProcQ(&parent->p_child) && parent->p_child.next)
+      /* Cycle through the children and remove from them from the ready queue */
+      list_for_each(pos, &parent->p_child)
+        list_add_tail(
+          &outProcQ(&ready_queue, list_entry(pos, pcb_t, p_sib))->p_next, &q);
+  }
+
+  /* At this point all the terminated processes may be freed with freePcb.
+   * However, this function is going to work very differently in the next phase
+   * so it probably doesn't matter too much.
+   */
+
+  /* Run the scheduler to carry on with the execution */
+  scheduler();
 }
--- phase1.5/E/sysbp.e
+++ phase1.5/E/sysbp.e
@@ -1,10 +1,10 @@
 #ifndef PHASE15_SYSBP_E_
 #define PHASE15_SYSBP_E_
 
-/* Initialize New Area for system calls/breakpoints */
-extern void sysbp_init();
-
 /* Main handler for system calls and breakpoints */
 extern void sysbp();
+
+/* Handler for sys3: terminate current process and its tree */
+extern void sys3();
 
 #endif
